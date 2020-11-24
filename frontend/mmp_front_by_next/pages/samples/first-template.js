bf0557de16f01fb3153eefc489897209a94c8b3d
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-11-24 19:30:25 +0900

    add some css

--- frontend/mmp_front_by_next/pages/samples/first-template.js
+++ frontend/mmp_front_by_next/pages/samples/first-template.js
@@ -1,3 +1,25 @@
+import styles from '../../styles/forTemplate/first.module.css'
+
 export default function FirstTemplate() {
-    return <h1> First Post </h1>
+    return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+    )
 }
