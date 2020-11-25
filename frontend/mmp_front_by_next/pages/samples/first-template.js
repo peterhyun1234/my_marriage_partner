02e4a2632b0d4575b593754168f6c253b278442b
peterhyun <peterhyun1234@gmail.com>
2020-11-25 18:20:55 +0900

    add library for UI

--- frontend/mmp_front_by_next/pages/samples/first-template.js
+++ frontend/mmp_front_by_next/pages/samples/first-template.js
@@ -1,13 +1,29 @@
 import styles from '../../styles/forTemplate/first.module.css'
+import { Button } from '@material-ui/core';
 
 export default function FirstTemplate() {
     return (
     <div className={styles.container}>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.bigBox}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+          <Button variant="contained">Default</Button>
+          <Button variant="contained" color="primary">
+            Primary
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary
+          </Button>
+          <Button variant="contained" disabled>
+            Disabled
+          </Button>
+          <Button variant="contained" color="primary" href="#contained-buttons">
+            Link
+          </Button>
+        </div>
       </main>
 
       <footer className={styles.footer}>
