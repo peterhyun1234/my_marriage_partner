353a1063357d098e7952f6e0ebfc02b4ca3264cd
peterhyun <peterhyun1234@gmail.com>
2020-11-25 18:48:13 +0900

    add some componets for invatation sample

--- frontend/mmp_front_by_next/pages/samples/first-template.js
+++ frontend/mmp_front_by_next/pages/samples/first-template.js
@@ -8,6 +8,55 @@ export default function FirstTemplate() {
       <main className={styles.main}>
         <div className={styles.bigBox}>
           <h1 className={styles.title}>
+            wedding invitation
+          </h1>
+          <p>
+            11.28
+          </p>
+          <p>
+            전현빈 그리고 홍길동
+          </p>
+          <p>
+            (메인 사진)
+          </p>
+          <p>
+            2020년 11월 28일(토) 오후 3:30
+          </p>
+          <p>
+            만동우프리미엄 / 3층 블라썸홀
+          </p>
+        </div>
+        <div className={styles.smallBox}>
+          <h1 className={styles.title}>
+            초대합니다
+          </h1>
+          <p>invatation</p>          
+          <br></br>
+          <p>뭐라머라 와서 축하해주세요. 뭐라머라 와서 축하해주세요. 뭐라머라 와서 축하해주세요. 뭐라머라 와서 축하해주세요. 뭐라머라 와서 축하해주세요.</p>          
+          <hr width ="90%" className={styles.sampleLine}></hr>
+          <p>신랑 홍길동 + (전화 아이콘)</p>          
+          <p>신부 전현빈 + (전화 아이콘)</p>          
+        </div>
+        <div className={styles.bigBox}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+          <Button variant="contained">Default</Button>
+          <Button variant="contained" color="primary">
+            Primary
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary
+          </Button>
+          <Button variant="contained" disabled>
+            Disabled
+          </Button>
+          <Button variant="contained" color="primary" href="#contained-buttons">
+            Link
+          </Button>
+        </div>
+        <div className={styles.smallBox}>
+          <h1 className={styles.title}>
             Welcome to <a href="https://nextjs.org">Next.js!</a>
           </h1>
           <Button variant="contained">Default</Button>
