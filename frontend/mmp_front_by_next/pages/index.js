90b9547dde8f31c110fa7b6f1ae27fea9fe6b05c
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-12-12 15:24:11 +0900

    add some modules

--- frontend/mmp_front_by_next/pages/index.js
+++ frontend/mmp_front_by_next/pages/index.js
@@ -1,68 +1,237 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}));
+
+
+
 export default function Home() {
+
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={classes.root}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+    // <div>
+    //   {['left'].map((anchor) => (
+    //     <React.Fragment key={anchor}>
+    //       <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+    //       <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+    //         {list(anchor)}
+    //       </Drawer>
+    //     </React.Fragment>
+    //   ))}
+    // </div>
+    
+  );
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  // return (
+  //   <div className={styles.container}>
+  //     <Head>
+  //       <title>Create Next App</title>
+  //       <link rel="icon" href="/favicon.ico" />
+  //     </Head>
 
-        <div className={styles.grid}>
-        <Link href="/samples/first-template">
-            <a
-              className={styles.card}
-            >
-              <h3>First-sample &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-          </Link>
+  //     <main className={styles.main}>
+  //       <h1 className={styles.title}>
+  //         Welcome to <a href="https://nextjs.org">Next.js!</a>
+  //       </h1>
 
-          <Link href="/samples/second-template">
-            <a
-              className={styles.card}
-            >
-              <h3>Second-sample &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-          </Link>
+  //       <p className={styles.description}>
+  //         Get started by editing{' '}
+  //         <code className={styles.code}>pages/index.js</code>
+  //       </p>
+
+  //       <div className={styles.grid}>
+  //       <Link href="/samples/first-template">
+  //           <a
+  //             className={styles.card}
+  //           >
+  //             <h3>First-sample &rarr;</h3>
+  //             <p>Discover and deploy boilerplate example Next.js projects.</p>
+  //           </a>
+  //         </Link>
+
+  //         <Link href="/samples/second-template">
+  //           <a
+  //             className={styles.card}
+  //           >
+  //             <h3>Second-sample &rarr;</h3>
+  //             <p>Discover and deploy boilerplate example Next.js projects.</p>
+  //           </a>
+  //         </Link>
           
-          <Link href="/samples/third-template">
-            <a
-              className={styles.card}
-            >
-              <h3>Third-sample &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-          </Link>
+  //         <Link href="/samples/third-template">
+  //           <a
+  //             className={styles.card}
+  //           >
+  //             <h3>Third-sample &rarr;</h3>
+  //             <p>Discover and deploy boilerplate example Next.js projects.</p>
+  //           </a>
+  //         </Link>
 
       
 
-        </div>
-      </main>
+  //       </div>
+  //     </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  //     <footer className={styles.footer}>
+  //       <a
+  //         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Powered by{' '}
+  //         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+  //       </a>
+  //     </footer>
+  //   </div>
+  // )
 }
