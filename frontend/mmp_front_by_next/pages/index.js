179f9b311be4e118709ff257c88461de53b60b6c
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-12-13 14:31:59 +0900

    add main page

--- frontend/mmp_front_by_next/pages/index.js
+++ frontend/mmp_front_by_next/pages/index.js
@@ -220,6 +220,62 @@ export default function Home() {
           )}
         </Toolbar>
       </AppBar>
+      <Head>
+        <title>My Marriage Partner</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <Link href="/samples/first-template">
+            <a
+              className={styles.smallBox}
+            >
+              <h3>First-sample &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+          </Link>
+
+          <Link href="/samples/second-template">
+            <a
+              className={styles.smallBox}
+            >
+              <h3>Second-sample &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+          </Link>
+          
+          <Link href="/samples/third-template">
+            <a
+              className={styles.smallBox}
+            >
+              <h3>Third-sample &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+          </Link>
+
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
     
   );
